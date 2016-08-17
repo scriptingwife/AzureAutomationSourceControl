69c7c1adacb8257b6b3977018048eeec1efc1b7f
scriptingwife <scriptingwife@hotmail.com>
2016-08-17 09:42:27 +0200

    Created StartAllV2Vms.ps1 in AzureAutomation.

---
+++ StartAllV2Vms.ps1
@@ -0,0 +1,9 @@
+workflow StartAllV2Vms { 
+	$Conn = Get-AutomationConnection -Name AzureRunAsConnection 
+ 	Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+ 	-ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+	$VMsv2 = Get-AzureRmVM
+	foreach -Parallel ($vm in $vmsv2)
+    	{Start-AzureRmVM -Name $vm.name -ResourceGroupName 'server'}
+}
